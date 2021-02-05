c2e542db98456089a343771d2ea4d45c4d3e9b5b
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2021-02-05 13:45:24 +0100

    Avoid blank messages

--- comment_list_bridge.py
+++ comment_list_bridge.py
@@ -4,6 +4,8 @@ class Comment:
     def __init__(self, message):
         self.author = Author(message.user.name)
         self.body = message.text
+        if (len(self.body) == 0):
+            self.body = '...'
         self.score = 0
 
 class Author:
